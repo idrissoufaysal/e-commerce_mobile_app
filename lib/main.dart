eeb1574ffa26741fd47e9963f2c165aee27ccab3
idrissoufaysal93 <idrissoufaysal93@gmail.com>
2024-10-05 01:07:38 +0000

    cart interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'providers/cart_provider.dart';
+import 'providers/favorite_provider.dart';
 import 'screens/na_bar_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,18 +12,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: BottomNavBar(),
-      
-    );
-  }
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          // for add to cart
+          ChangeNotifierProvider(create: (_) => CartProvider()),
+          // for favorite
+          ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            textTheme: GoogleFonts.mulishTextTheme(),
+          ),
+          home: const BottomNavBar(),
+        ),
+      );
 }
